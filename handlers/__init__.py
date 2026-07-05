8619ea98bf805f0167a928224304f9c01c8e390d
akbar1301 <52690233+akbar1301@users.noreply.github.com>
2026-07-05 12:08:32 +0800

    Delete handlers directory

--- handlers/__init__.py
+++
@@ -1,6 +0,0 @@
-from .admin import lunas, rekap, setkoordinat
-from .checkin import checkin_start, photo_received, location_received
-from .daftar import daftar
-from .location_tracking import handle_location
-from .start import start
-from .status import status
